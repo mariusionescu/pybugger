1a4907d928119832b5d9906ee41cb85e5747f7de
Marius Ionescu <marius@mi.www.ro>
2022-02-17 12:56:38 +0200

    Removed ipdb dependecy

--- pybugger.py
+++ pybugger.py
@@ -1,5 +1,4 @@
 import inspect
-import ipdb
 
 
 def is_printable(obj):
@@ -14,7 +13,7 @@ def get_prev(frame):
     lines = readlines(frame_info.filename)
     l = ''
     i = 2
-    while not l:
+    while not l and lines:
         l = lines[frame_info.lineno - i]
         i += 1
     return l
@@ -26,18 +25,14 @@ def debug(message=None):
     frame = frame_data[0]
 
     prev = get_prev(frame)
-    print("[PYBUGGER] START TRACE -----------------------------------------------------------------")
-    print("[PYBUGGER] [PREV] {}".format(prev))
-    print("[PYBUGGER] [LOCALS]")
+
+    print("\n[PYBUGGER] ********************************************************************************")
+    print("[PYBUGGER]     line|  {}".format(prev))
     for l_n, l_v in frame.f_locals.items():
         if not l_n.startswith('_') and is_printable(l_v):
-            print("[PYBUGGER]     {}: {}".format(l_n, l_v))
-    print("[PYBUGGER] [GLOBALS]")
+            print("[PYBUGGER]    local|  {}: {}".format(l_n, l_v))
     for g_n, g_v in frame.f_globals.items():
         if not g_n.startswith('_') and is_printable(g_v):
-            print("[PYBUGGER]     {}: {}".format(g_n, g_v))
-    print("[PYBUGGER] END TRACE -------------------------------------------------------------------")
-    #ipdb.set_trace()
-
-
+            print("[PYBUGGER]   global|  {}: {}".format(g_n, g_v))
+    print("[PYBUGGER] ********************************************************************************\n")
 
